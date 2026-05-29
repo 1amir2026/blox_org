a1f92710d4da076b257906d741190cb083fb92e3
1amir2026 <abdlrasolmofidi424@gmail.com>
2026-05-29 22:59:25 +0330

    Add files via upload

--- keyboards/main.py
+++ keyboards/main.py
@@ -3,10 +3,10 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 main_menu = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="🔗 لینک رفرال")],
-        [KeyboardButton(text="👤 مشخصات من")],
         [KeyboardButton(text="❓ سوالات متداول")],
         [KeyboardButton(text="🖼 درخواست پروفایل")],
-        [KeyboardButton(text="📞 پشتیبانی")]
+        [KeyboardButton(text="📞 پشتیبانی")],
+        [KeyboardButton(text="👤 مشخصات من")]
     ],
     resize_keyboard=True
 )
