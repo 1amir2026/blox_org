56950738eab23abd5a06649276298a15641c4cf3
1amir2026 <abdlrasolmofidi424@gmail.com>
2026-05-29 22:05:09 +0330

    Add files via upload

--- keyboards/main.py
+++ keyboards/main.py
@@ -5,6 +5,7 @@ main_menu = ReplyKeyboardMarkup(
         [KeyboardButton(text="🔗 لینک رفرال")],
         [KeyboardButton(text="👤 مشخصات من")],
         [KeyboardButton(text="❓ سوالات متداول")],
+        [KeyboardButton(text="🖼 درخواست پروفایل")],
         [KeyboardButton(text="📞 پشتیبانی")]
     ],
     resize_keyboard=True
