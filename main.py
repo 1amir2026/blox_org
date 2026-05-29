3b2f216be851a9a4c5e12decdcce4dffbdda1702
1amir2026 <abdlrasolmofidi424@gmail.com>
2026-05-29 22:53:31 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ from handlers.profile import router as profile_router
 from handlers.referral import router as referral_router
 from handlers.faq import router as faq_router
 from handlers.support import router as support_router
-
-from database.models import init_db
+from handlers.info import router as info_router
 
 load_dotenv()
 
@@ -19,22 +18,15 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-# Router ها
 dp.include_router(start_router)
 dp.include_router(profile_router)
 dp.include_router(referral_router)
 dp.include_router(faq_router)
 dp.include_router(support_router)
-
+dp.include_router(info_router)
 
 async def main():
-
-    # ساخت دیتابیس
-    await init_db()
-
-    print("✅ Bot Started")
-
-    # اجرای ربات
+    print("✅ ربات روشن شد...")
     await dp.start_polling(bot)
 
 
